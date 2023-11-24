285627829f2e9fa71e6c46e5e39ec718dfcf5861
Patryk <s090120@student.tu.kielce.pl>
2023-11-24 09:47:53 +0100

    Added documentation with Sphinx for backend

--- backend/mainApp/serializers.py
+++ backend/mainApp/serializers.py
@@ -2,6 +2,16 @@ from rest_framework.serializers import ModelSerializer
 from .models import Comment
 
 class CommentSerializer(ModelSerializer):
+    """
+    Serializer dla modelu Comment.
+
+    Mapuje pola modelu Comment na format JSON i obsługuje walidację danych wejściowych.
+
+    Fields:
+    - title (str): Tytuł komentarza.
+    - date_time (datetime): Data i czas utworzenia komentarza.
+    - comment (str): Treść komentarza.
+    """
     class Meta:
         model = Comment
         fields = ['title', 'date_time', 'comment']
